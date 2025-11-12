c9d2a0dd5525af3124ac73d9558509cfccf29ac2
Ксения Ерёмина <what.about.ksenia@gmail.com>
2025-11-12 23:12:22 +0300

    fix second task

--- second_task.py
+++ second_task.py
@@ -48,18 +48,15 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-
+# Проверка работы кода
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
 student = Student('Алёхина', 'Ольга', 'Ж')
-
 student.courses_in_progress += ['Python', 'Java']
 lecturer.courses_attached += ['Python', 'C++']
 reviewer.courses_attached += ['Python', 'C++']
-
-print(student.rate_lecture(lecturer, 'Python', 7))  # None
-print(student.rate_lecture(lecturer, 'Java', 8))  # Ошибка
-print(student.rate_lecture(lecturer, 'С++', 8))  # Ошибка
-print(student.rate_lecture(reviewer, 'Python', 6))  # Ошибка
-
-print(lecturer.grades)  # {'Python': [7]}
+print(student.rate_lecture(lecturer, 'Python', 7))  
+print(student.rate_lecture(lecturer, 'Java', 8))  
+print(student.rate_lecture(lecturer, 'С++', 8))  
+print(student.rate_lecture(reviewer, 'Python', 6))  
+print(lecturer.grades)  
